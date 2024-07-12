64793905eb0292d0a1132ca8585441b9cb4a1f72
Ubuntu <ubuntu@ip-172-31-8-189.ap-south-1.compute.internal>
2024-07-12 09:37:49 +0000

    updated Security Groups for Nodes

--- secutity-groups.tf
+++ secutity-groups.tf
@@ -39,6 +39,7 @@ resource "aws_security_group" "k8-sg-common" {
 
 }
 
+#flannel udp rules cg
 
 resource "aws_security_group" "k8-udp" {
   name = "flannel-overlay-backend"
@@ -60,6 +61,92 @@ resource "aws_security_group" "k8-udp" {
     from_port = 8472
     to_port =  8472
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+# control plane ports sg
+
+resource "aws_security_group" "k8-sg-control-plane" {
+  name = "kubeadm-control-plane security group"
+  ingress {
+    description = "API Server"
+    protocol = "tcp"
+    from_port = 6443
+    to_port = 6443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "etcd server client API"
+    protocol = "tcp"
+    from_port = 2379
+    to_port = 2380
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Kubelet API"
+    protocol = "tcp"
+    from_port = 10250
+    to_port = 10250
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Kube Scheduler"
+    protocol = "tcp"
+    from_port = 10259
+    to_port = 10259
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Kube Contoller Manager"
+    protocol = "tcp"
+    from_port = 10257
+    to_port = 10257
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Control Plane SG"
+  }
+}
+
+
+#worker node sg group
+
+resource "aws_security_group" "k8-sg-worker" {
+  name = "worker-node security group"
+
+  ingress {
+		description = "Kubelet API"
+		protocol = "tcp"
+		from_port = 10250
+		to_port = 10250
+		cidr_blocks = ["0.0.0.0/0"]
+
+	}
+
+  ingress {
+    description = "NodePort services"
+    protocol = "tcp"
+    from_port = 30000
+    to_port = 32767
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+	ingress {
+		description = "kube-proxy"
+		protocol = "tcp"
+		from_port = 10256
+		to_port = 10256
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+  tags = {
+    Name = "Worker Nodes SG"
   }
 
 }
